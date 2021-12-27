334ab4cd2e00f16aec10c72b54e49a889f304580
Jonthelepreeedcchaun <43424786+Jonthelepreeedcchaun@users.noreply.github.com>
2021-12-26 19:03:11 -0800

    outside apartment revamped

--- src/Logic_Code/FallThroughSlopes.gd
+++ src/Logic_Code/FallThroughSlopes.gd
@@ -4,13 +4,15 @@ var wait = 0
 var is_on_slope = false
 var dis = false
 
+var railing_order = 4 # order of the railing in the scene
+
 func fallThrough():
 	is_on_slope = get_parent().get_parent().get_node("Matt").get_node("KinematicMatt").surface_property == "slope"
 	if Input.is_action_pressed("down") and Input.is_action_just_pressed("space") and is_on_slope: ##if down and jump is pressed
 		if get_parent().get_parent().has_node("Matt"):
 			get_parent().get_parent().get_node("Matt").get_node("KinematicMatt").surface_property = "default"
 		dis = true
-		get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), 2)
+		get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), railing_order - 1)
 
 func check_dis():
 	disabled = dis
@@ -21,8 +23,8 @@ func _process(delta: float) -> void:
 
 func _on_On_body_entered(body: Node) -> void:
 	dis = false
-	get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), 3)
+	get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), railing_order)
 
 func _on_Off_body_entered(body: Node) -> void:
 	dis = true
-	get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), 2)
+	get_parent().get_parent().move_child(get_parent().get_parent().get_node("railing"), railing_order - 1)
