8fb10ea5bff6009f45f8bf1c4ee0fb55540e2ede
Jonthelepreeedcchaun <43424786+Jonthelepreeedcchaun@users.noreply.github.com>
2022-03-28 11:36:57 -0700

    fixed it
    told you i would
    implemented it on regular matt as well
    
    dialogue works a lot more smoothly now

--- src/matt/DialogueArea.gd
+++ src/matt/DialogueArea.gd
@@ -2,7 +2,14 @@ extends Area2D
 
 func _on_DialogueArea_body_entered(body: Node) -> void:
 	get_parent().get_node("DialogueLogic").person_list.append(body.name)
-	print(body.name)
 
 func _on_DialogueArea_body_exited(body: Node) -> void:
 	get_parent().get_node("DialogueLogic").person_remove.append(body.name)
+
+var mattPos
+func area_follow(delta):
+	mattPos = get_parent().get_node("KinematicMatt").mattPosition
+	position = mattPos
+
+func _process(delta: float) -> void:
+	area_follow(delta)
